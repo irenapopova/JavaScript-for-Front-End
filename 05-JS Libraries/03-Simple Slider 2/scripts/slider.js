9879929bd1bb0b6a9eb69e925385b966c54005b1
irenapopova <irenejpopova6@gmail.com>
2019-09-22 12:48:11 +0200

    JS Libraries

---
+++ 05-JS Libraries/03-Simple Slider 2/scripts/slider.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $(".autoplay").slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 5000
+    });
+});
